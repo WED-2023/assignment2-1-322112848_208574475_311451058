723030b89902a999c881a41b003d816ee84e115e
Ofir Horn <ofirhor@post.bgu.ac.il>
2024-06-24 11:42:40 +0300

    adding .map files

---
+++ dist/js/381.3ba0d911.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkassignment_2_1"]=self["webpackChunkassignment_2_1"]||[]).push([[381],{1381:(s,t,n)=>{n.r(t),n.d(t,{default:()=>r});var e=function(){var s=this;s._self._c;return s._m(0)},a=[function(){var s=this,t=s._self._c;return t("div",{staticClass:"container"},[t("h1",{staticClass:"title"},[s._v("Search Page")])])}],l=n(1656),c={},i=(0,l.A)(c,e,a,!1,null,null,null);const r=i.exports}}]);
+//# sourceMappingURL=381.3ba0d911.js.map
