d03277892d6995ccb2d07d164145833d7354f704
Ofir Horn <ofirhor@post.bgu.ac.il>
2024-06-24 11:01:33 +0300

    Addtion pages

---
+++ src/pages/MyRecipePage.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>My Recipes</h1>
+    <AddRecipeModal @addRecipe="handleAddRecipe" />
+    <RecipePreviewList title="My Recipes" :recipes="recipes" />
+  </div>
+</template>
+
+<script>
+import RecipePreviewList from '../components/RecipePreviewList.vue';
+
+export default {
+  components: {
+    // AddRecipeModal,
+    RecipePreviewList
+  },
+  data() {
+    return {
+      recipes: [],
+      currentMaxId: 0
+    };
+  },
+  methods: {
+    handleAddRecipe(newRecipe) {
+      newRecipe.id = Date.now();
+      this.recipes.push(newRecipe);
+      this.currentMaxId = newRecipe.id;
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
